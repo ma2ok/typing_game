4e218bd91a3603153c2d9ba3f0f2a73f4c4308ba
Tubasa Matsuoka <workwing0283@gamil.com>
2020-04-16 15:54:23 +0900

    ちゃんとカウントダウンできた

--- js/main.js
+++ js/main.js
@@ -1,16 +1,24 @@
 'use strict';
 
 {
-  const word = 'apple';
+  const words = [
+    'apple',
+    'sky',
+    'blue',
+    'middle',
+    'set'
+  ];
+  let word = words[Math.floor(Math.random() * words.length)];
   let loc = 0;
   let score = 0;
   let miss = 0;
+  const timeLimit = 3 * 1000;
+  let startTime;
 
   const target = document.getElementById('target');
   const scoreLabel = document.getElementById('score');
   const missLabel = document.getElementById('miss');
-
-  target.textContent = word;
+  const timerLabel = document.getElementById('timer');
 
   function updateTarget() {
     let placeholder = '';
@@ -20,9 +28,34 @@
     target.textContent = placeholder + word.substring(loc);
   }
 
+  function updateTimer(){
+    const timeLeft =startTime + timeLimit -Date.now();
+    timerLabel.textContent = (timeLeft/1000).toFixed(2);
+
+    const timeoutId = setTimeout(() =>{
+      updateTimer();
+    },10);
+
+    if (timeLeft <0){
+      clearTimeout(timeoutId);
+      alert('Game Over');
+    }
+  }
+
+  window.addEventListener('click', () =>{
+    target.textContent = word;
+    startTime = Date.now();
+    updateTimer();
+  }
+  )
+
   window.addEventListener('keydown', e => {
     if (e.key === word[loc]) {
       loc++;
+      if (loc === word.length){
+        word = words[Math.floor(Math.random() * words.length)];
+        loc = 0;
+      }
       updateTarget();
       score++;
       scoreLabel.textContent = score;
