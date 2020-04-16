dad84929293c89028b6f5b709a3d45be40031335
Tubasa Matsuoka <workwing0283@gamil.com>
2020-04-16 16:05:40 +0900

    ちゃんとOKになったもの

--- js/main.js
+++ js/main.js
@@ -14,6 +14,8 @@
   let miss = 0;
   const timeLimit = 3 * 1000;
   let startTime;
+  let isPlaying = false;
+
 
   const target = document.getElementById('target');
   const scoreLabel = document.getElementById('score');
@@ -37,6 +39,7 @@
     },10);
 
     if (timeLeft <0){
+      isPlaying = false;
       clearTimeout(timeoutId);
       timerLabel.textContent ='0.00';
       setTimeout(() =>{
@@ -46,6 +49,11 @@
   }
 
   window.addEventListener('click', () =>{
+    if(isPlaying === true){
+      return;
+    }
+    isPlaying = true;
+
     target.textContent = word;
     startTime = Date.now();
     updateTimer();
@@ -53,6 +61,9 @@
   )
 
   window.addEventListener('keydown', e => {
+    if (isPlaying !== true){
+      return;
+    }
     if (e.key === word[loc]) {
       loc++;
       if (loc === word.length){
